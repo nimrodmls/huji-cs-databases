9dee1dc6b3a719e603fef622ca38b143eac7cc72
Nimrod Mallis <nimrodmallis@gmail.com>
2024-06-29 18:08:19 +0300

    Fixes to the correct & example sqls, also presubmit fixes

--- ex3/q2.sql
+++ ex3/q2.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT A.name, I.institution
+SELECT DISTINCT I.institution, A.name
 FROM authors A NATURAL JOIN institutions I
 WHERE (A.institution = I.institution) AND (I.region = 'africa')
 ORDER BY I.institution, A.name ASC;
--- ex3/q3.sql
+++ ex3/q3.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT A.name, I.institution
+SELECT DISTINCT I.institution, A.name
 FROM authors A NATURAL JOIN institutions I NATURAL JOIN conferences C
 WHERE (I.country = 'il') AND (A.totalcount >= 2) AND (C.conference LIKE 'sig%')
 ORDER BY A.name, I.institution ASC;
