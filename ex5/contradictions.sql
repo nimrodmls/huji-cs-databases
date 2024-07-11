5aebcb23613b8bf45f9f80651f10a2f93acfe928
Nimrod Mallis <nimrodmallis@gmail.com>
2024-07-11 18:37:37 +0300

    Added the SQL for Question 2

---
+++ ex5/contradictions.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT TransactionNo, ProductNo
+FROM sales S1, sales S2
+WHERE 
+    (S1.TransactionNo = S2.TransactionNo AND S1.CustomerNo <> S2.CustomerNo) OR
+    (S1.TransactionNo = S2.TransactionNo AND S1.Date <> S2.Date) OR
+    (S1.CustomerNo = S2.CustomerNo AND S1.Country <> S2.Country) OR
+    (S1.ProductNo = S2.ProductNo AND S1.ProductName <> S2.ProductName)
+ORDER BY TransactionNo, ProductNo ASC;
