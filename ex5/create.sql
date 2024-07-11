78ec5dac3189c7a66227fa4cf99836b5fda2bfb6
Nimrod Mallis <nimrodmallis@gmail.com>
2024-07-11 17:55:48 +0300

    Added files from moodle

---
+++ ex5/create.sql
@@ -0,0 +1,10 @@
+create table sales(
+	TransactionNo varchar,
+	Date varchar,
+	ProductNo varchar,
+	ProductName varchar,
+	Price varchar,
+	Quantity varchar,
+	CustomerNo varchar,
+	Country varchar
+);
